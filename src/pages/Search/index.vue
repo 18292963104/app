cc4392f1634fd84843e2eaa0c87011208179afb6
miliya <604676160@qq.com>
2023-02-06 18:18:42 +0800

    code change on 0206

--- src/pages/Search/index.vue
+++ src/pages/Search/index.vue
@@ -62,35 +62,7 @@
               </li>
             </ul>
           </div>
-          <div class="fr page">
-            <div class="sui-pagination clearfix">
-              <ul>
-                <li class="prev disabled">
-                  <a href="#">«上一页</a>
-                </li>
-                <li class="active">
-                  <a href="#">1</a>
-                </li>
-                <li>
-                  <a href="#">2</a>
-                </li>
-                <li>
-                  <a href="#">3</a>
-                </li>
-                <li>
-                  <a href="#">4</a>
-                </li>
-                <li>
-                  <a href="#">5</a>
-                </li>
-                <li class="dotted"><span>...</span></li>
-                <li class="next">
-                  <a href="#">下一页»</a>
-                </li>
-              </ul>
-              <div><span>共10页&nbsp;</span></div>
-            </div>
-          </div>
+          <Pagination />
         </div>
       </div>
     </div>
@@ -99,12 +71,14 @@
 
 <script>
   import SearchSelector from './SearchSelector/SearchSelector'
+  import Pagination from '../../components/Pagination'
   import { mapGetters } from 'vuex';
   export default {
     name: 'Search',
 
     components: {
-      SearchSelector
+      SearchSelector,
+      Pagination
     },
     data(){
       return {
@@ -114,7 +88,7 @@
           "category3Id": "",
           "categoryName": "",
             "keyword": "",
-            "order": "1:desc",
+            "order": "2:asc",
             "pageNo": 1,
             "pageSize": 10,
             "props": [],
@@ -178,8 +152,14 @@
         this.getData();
       },
       changeOrder(flag){
-        this.searchParams.order.split(':')[0]
-        this.searchParams.order.split(':')[1]
+        const [ originFlag, originSort ] = this.searchParams.order.split(':')
+        let newOrder = ''
+        if (flag === originFlag) {
+          newOrder = `${originFlag}:${originSort === 'desc' ?  'asc' : 'desc'}`
+        } else {
+          newOrder = `${flag}:${'desc'}`
+        }
+        this.searchParams.order =  newOrder
       }
     },
     watch: {
@@ -433,93 +413,6 @@
                   }
                 }
               }
-            }
-          }
-        }
-
-        .page {
-          width: 733px;
-          height: 66px;
-          overflow: hidden;
-          float: right;
-
-          .sui-pagination {
-            margin: 18px 0;
-
-            ul {
-              margin-left: 0;
-              margin-bottom: 0;
-              vertical-align: middle;
-              width: 490px;
-              float: left;
-
-              li {
-                line-height: 18px;
-                display: inline-block;
-
-                a {
-                  position: relative;
-                  float: left;
-                  line-height: 18px;
-                  text-decoration: none;
-                  background-color: #fff;
-                  border: 1px solid #e0e9ee;
-                  margin-left: -1px;
-                  font-size: 14px;
-                  padding: 9px 18px;
-                  color: #333;
-                }
-
-                &.active {
-                  a {
-                    background-color: #fff;
-                    color: #e1251b;
-                    border-color: #fff;
-                    cursor: default;
-                  }
-                }
-
-                &.prev {
-                  a {
-                    background-color: #fafafa;
-                  }
-                }
-
-                &.disabled {
-                  a {
-                    color: #999;
-                    cursor: default;
-                  }
-                }
-
-                &.dotted {
-                  span {
-                    margin-left: -1px;
-                    position: relative;
-                    float: left;
-                    line-height: 18px;
-                    text-decoration: none;
-                    background-color: #fff;
-                    font-size: 14px;
-                    border: 0;
-                    padding: 9px 18px;
-                    color: #333;
-                  }
-                }
-
-                &.next {
-                  a {
-                    background-color: #fafafa;
-                  }
-                }
-              }
-            }
-
-            div {
-              color: #333;
-              font-size: 14px;
-              float: right;
-              width: 241px;
             }
           }
         }
